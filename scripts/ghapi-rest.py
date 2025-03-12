1df6bd247c815e9b45448691da3dddaf6f06e26e
bubutz <guanpok86@hotmail.com>
2025-03-13 07:08:23 +0800

    test rest

--- scripts/ghapi-rest.py
+++ scripts/ghapi-rest.py
@@ -7,16 +7,20 @@ import requests
 def main():
     response = requests.get(URL, headers=headers)
     print("--- FULL ---------------------------------------------")
-    for line in response.json():
-        print(line)
-    # print("--- FULL ---------------------------------------------")
-    # print("--- FULL ---------------------------------------------")
+    for branch in response.json():
+        print(branch)
+        print("    --- BRANCHNAME -----------------------------------")
+        if branch["name"] == new_branch_name:
+            print("Hit ", branch["name"])
+        else:
+            print("Miss", branch["name"])
     # print("--- FULL ---------------------------------------------")
 
 
 if __name__ == '__main__':
     TOK = sys.argv[1]
     URL = sys.argv[2]
+    new_branch_name = sys.argv[3]
 
     headers = {
         "Authorization": f"Bearer {TOK}",
