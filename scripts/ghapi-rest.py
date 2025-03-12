84012efec0bab192cf032141414058be638ec64a
bubutz <guanpok86@hotmail.com>
2025-03-13 06:58:44 +0800

    test rest

---
+++ scripts/ghapi-rest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+
+
+def main():
+    print("TOK:", TOK)
+    print("URL:", URL)
+
+    response = requests.get(URL, headers=headers).json()
+    print(response)
+
+
+if __name__ == '__main__':
+    TOK = sys.argv[1]
+    URL = sys.argv[2]
+
+    headers = {
+        "Authorization": f"Bearer {TOK}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    main()
