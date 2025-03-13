e63a870a85be74933d4f7382950b561bd8eb3118
bubutz <guanpok86@hotmail.com>
2025-03-13 08:34:25 +0800

    test rest

--- scripts/ghapi-rest.py
+++ scripts/ghapi-rest.py
@@ -5,22 +5,28 @@ import requests
 
 
 def main():
-    response = requests.get(URL, headers=headers)
-    print("--- FULL ---------------------------------------------")
-    print(response.json())
-    for branch in response.json():
-        print("    --- BRANCHNAME -----------------------------------")
-        if branch["name"] == new_branch_name:
-            print("Hit ", branch["name"])
-        else:
-            print("Miss", branch["name"])
+    # response = requests.get(URL, headers=headers)
     # print("--- FULL ---------------------------------------------")
+    # print(response.json())
+    # for branch in response.json():
+    #     print("    --- BRANCHNAME -----------------------------------")
+    #     if branch["name"] == new_branch_name:
+    #         print("Hit ", branch["name"])
+    #     else:
+    #         print("Miss", branch["name"])
+    # print("--- FULL ---------------------------------------------")
+    for branch_name in new_branch_name:
+        url = f'https://api.github.com/repos/bubutz/testops/branches/{branch_name}/protection'
+        response = requests.get(URL, headers=headers)
+        print(response.json())
+        print()
 
 
 if __name__ == '__main__':
     TOK = sys.argv[1]
     URL = sys.argv[2]
-    new_branch_name = sys.argv[3]
+    # new_branch_name = sys.argv[3]
+    new_branch_name = ['foo', 'main', 'release']
 
     headers = {
         "Authorization": f"Bearer {TOK}",
