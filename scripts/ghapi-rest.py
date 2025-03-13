12262e727fec269c59f333ae47d4ad066b1b8eba
bubutz <guanpok86@hotmail.com>
2025-03-13 09:02:12 +0800

    upd changeset

--- scripts/ghapi-rest.py
+++ scripts/ghapi-rest.py
@@ -15,6 +15,11 @@ def main():
     #     else:
     #         print("Miss", branch["name"])
     # print("--- FULL ---------------------------------------------")
+    new_branch_name = []
+    fresponse = requests.get(URL, headers=headers)
+    for branch in fresponse.json():
+        new_branch_name.append(branch["name"])
+
     for branch_name in new_branch_name:
         print(branch_name)
         url = f'https://api.github.com/repos/bubutz/testops/branches/{branch_name}/protection'
@@ -28,7 +33,6 @@ if __name__ == '__main__':
     TOK = sys.argv[1]
     URL = sys.argv[2]
     # new_branch_name = sys.argv[3]
-    new_branch_name = ['foo', 'main', 'release/prod']
 
     headers = {
         "Authorization": f"Bearer {TOK}",
