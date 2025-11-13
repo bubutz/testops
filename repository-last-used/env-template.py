8397a4b982d08278e66def2b5f4e4236e50cb750
bubutz <guanpok86@hotmail.com>
2025-11-14 04:42:54 +0800

    rem

--- repository-last-used/env-template.py
+++
@@ -1,3 +0,0 @@
-EMAIL_ADDRESS = ""  # Atlassian Account email address
-BITBUCKET_API_TOKEN = ""  # Bitbucket API Token created with https://support.atlassian.com/bitbucket-cloud/docs/create-an-api-token/
-WORKSPACE = ""  # Cloud workspace slug/id (as seen in the url)
