e0d2ee25b893df84893acf5583743f8d1d423a1b
bubutz <guanpok86@hotmail.com>
2026-09-20 12:06:23 +0800

    test import

--- py_app/utils.py
+++ py_app/utils.py
@@ -1,8 +1,8 @@
-import sys
-from pathlib import Path
-ROOT_DIR = Path(__file__).resolve().parents[1]
-if str(ROOT_DIR) not in sys.path:
-    sys.path.insert(0, str(ROOT_DIR))
+# import sys
+# from pathlib import Path
+# ROOT_DIR = Path(__file__).resolve().parents[1]
+# if str(ROOT_DIR) not in sys.path:
+#     sys.path.insert(0, str(ROOT_DIR))
 from root_utils import printhello
 
 
