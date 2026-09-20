427f023e5546792751b9162e89a19dfe8e11916f
bubutz <guanpok86@hotmail.com>
2026-09-20 12:02:30 +0800

    test import

--- py_app/utils.py
+++ py_app/utils.py
@@ -1,4 +1,9 @@
-from ..root_utils import printhello
+import sys
+from pathlib import Path
+ROOT_DIR = Path(__file__).resolve().parents[1]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
+from root_utils import printhello
 
 
 def dostuff(i):
